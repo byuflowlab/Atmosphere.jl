3aa94396a8cb01003342075539120f354e5fcb8f
Judd Mehr <juddmehr@gmail.com>
2018-01-12 09:33:02 -0700

    update gravity constant, beautify, add gravity adjustment function.

--- src/properties.jl
+++ src/properties.jl
@@ -6,7 +6,9 @@ const Sc = 113.0  #constant in Sutherlands formula (K)
 const gamma = 1.4  #ratio of specific heats (air)
 const R_M = 287.0520809957016 #Molar Gas Constant: R (gas constant) = 8.31432e3 #N m kmol−1 K−1, M (mean molar mass) = 28.9645 #g/mol, R_M = R/M
 const earthradius = 6369.0e3 #radius of the earth (m)
-const gravity = 9.81
+const g = 9.80665 #gravitational constant at mean sea level
+
+
 """
 	atmospherefit(altitude::Float64)
 
@@ -27,6 +29,7 @@ function atmospherefit(altitude::Float64)
 
 end #atmospherefit
 
+
 """
 	temp_presfit(altitude::Float64)
 
@@ -45,6 +48,7 @@ function temp_presfit(altitude::Float64)
 	return T, P
 
 end #temp_presfit
+
 
 """
 	atmospheretable(altitude::Float64)
@@ -66,6 +70,7 @@ function atmospheretable(altitude::Float64)
 	return rho, mu, a
 
 end #atmospherefit
+
 
 """
 	temp_prestable(altitude::Float64)
@@ -92,14 +97,15 @@ function temp_prestable(altitude::Float64)
 
 	#find pressure
 	if temperaturegradient[idx] == 0.0
-		P = pressuretable[idx]*exp(-gravity*(altgeopot-altitudetable[idx])/R_M/temperaturetable[idx])
+		P = pressuretable[idx]*exp(-g*(altgeopot-altitudetable[idx])/R_M/temperaturetable[idx])
 	else
-		P = pressuretable[idx]*(temperaturetable[idx]/T)^(gravity/R_M/temperaturegradient[idx])
+		P = pressuretable[idx]*(temperaturetable[idx]/T)^(g/R_M/temperaturegradient[idx])
 	end
 
 	return T, P
 
 end
+
 
 """
 	density(Temperature::Float64, Pressure::Float64)
@@ -140,3 +146,22 @@ function viscosity(Temperature::Float64)
 	mu = musl*(Temperature/Tsl)^(3.0/2)*(Tsl+Sc)/(Temperature+Sc)
 	return mu
 end #viscosity()
+
+
+"""
+	gravity(altitude::Float64)
+
+Calculate adjusted gravitational acceleration at altitude (meters) and latitude (radians).
+
+1984 Ellipsoidal Gravity Formula.
+
+"""
+function gravity(altitude::Float64; latitude::Float64=0.7853981633974483)
+
+	#latitude adjustment of mean sea level gravitational acceleration.
+	adjlat = 9.7803253359*(1 + 0.00193185265241*sin(latitude)^2)/sqrt(1 - 0.00669437999013*sin(latitude)^2)
+	#altitude adjustment of gravitational constant.
+	adjalt = adjlat*(earthradius/(altitude+earthradius))^2
+
+	return adjalt
+end
