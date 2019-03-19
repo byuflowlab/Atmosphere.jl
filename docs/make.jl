431616671d30f229094628fe9d01c9130d432c36
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2019-03-18 19:11:25 -0600

    simplify documentation deployment

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,6 @@ using Documenter, Atmosphere
 makedocs(
     sitename = "Atmosphere",
     modules = [Atmosphere],
-    format=Documenter.HTML(prettyurls = get(ENV, "CI", nothing)=="true"),
     pages   = ["Home" => "index.md",
         "Example Usage" => "example.md",
         "Library" => "library.md"]
